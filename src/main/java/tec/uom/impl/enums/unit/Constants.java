4ec92da3d51cc010a38a1e81c24c0bbdfd7b93f8
Werner Keil <werner.keil@gmx.net>
2015-09-27 16:23:59 +0200

    21: Update to new systems
    Task-Url:
    https://github.com/unitsofmeasurement/uom-demos/issues/issue/21

--- src/main/java/tec/uom/impl/enums/unit/Constants.java
+++ src/main/java/tec/uom/impl/enums/unit/Constants.java
@@ -1,6 +1,6 @@
-/**
+/*
  *  Unit-API - Units of Measurement API for Java
- *  Copyright (c) 2005-2014, Jean-Marie Dautelle, Werner Keil, V2COM.
+ *  Copyright (c) 2005-2015, Jean-Marie Dautelle, Werner Keil, V2COM.
  *
  * All rights reserved.
  *
@@ -31,29 +31,4 @@ package tec.uom.impl.enums.unit;
 
 abstract class Constants {
 	static final char DEG = '\u00B0';
-	
-	static final double BYTE_FACTOR = 8.0;
-	
-    static final String BIT_NAME = "bit"; //$NON-NLS-1$
-    static final String BYTE_NAME = "Byte"; //$NON-NLS-1$
-	static final String KB_NAME = "kb"; //$NON-NLS-1$
-    static final String MB_NAME = "Mb"; //$NON-NLS-1$
-    static final String GB_NAME = "Gb"; //$NON-NLS-1$
-    static final String TB_NAME = "Tb"; //$NON-NLS-1$
-    static final String PB_NAME = "Pb"; //$NON-NLS-1$
-    static final String EB_NAME = "Eb"; //$NON-NLS-1$
-    static final String KBYTE_NAME = "kByte"; //$NON-NLS-1$
-    static final String MBYTE_NAME = "MByte"; //$NON-NLS-1$
-    static final String GBYTE_NAME = "GByte"; //$NON-NLS-1$
-    static final String TBYTE_NAME = "TByte"; //$NON-NLS-1$
-    static final String PBYTE_NAME = "PByte"; //$NON-NLS-1$
-    static final String EBYTE_NAME = "EByte"; //$NON-NLS-1$
-
-    static final String BPS_NAME = "Bit per Second"; //$NON-NLS-1$
-    static final String KBPS_NAME = "Kilobit per Second"; //$NON-NLS-1$
-    static final String MBPS_NAME = "Megabit per Second"; //$NON-NLS-1$
-    static final String GBPS_NAME = "Gigabit per Second"; //$NON-NLS-1$
-    static final String TBPS_NAME = "Terabit per Second"; //$NON-NLS-1$
-    static final String PBPS_NAME = "Petabit per Second"; //$NON-NLS-1$
-    static final String EBPS_NAME = "Exabit per Second"; //$NON-NLS-1$
 }
